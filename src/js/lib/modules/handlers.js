61117849fc74a7529c578a5aa44b211af1efaa83
Nikita <nikappleid@yandex.ru>
2022-03-08 22:09:09 +0300

    first

---
+++ src/js/lib/modules/handlers.js
@@ -0,0 +1,30 @@
+import $ from '../core';
+
+$.prototype.on = function(eventName, callback) {
+    if(!eventName || !callback) {
+        return this;
+    }
+    for(let i = 0; i < this.length; i++) {
+        this[i].addEventListener(eventName, callback);
+    }
+};
+
+$.prototype.off = function(eventName, callback) {
+    if(!eventName || !callback) {
+        return this;
+    }
+    for(let i = 0; i < this.length; i++) {
+        this[i].removeEventListener(eventName, callback);
+    }
+};
+
+
+$.prototype.click = function(handler) {
+    for(let i = 0; i < this.length; i++) {
+        if(handler) {
+            this[i].addEventListener('click', handler); 
+        } else {
+            this[i].click();
+        }
+    }
+};
